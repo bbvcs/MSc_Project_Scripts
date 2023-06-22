01bd13039ff478ae0083be2d0857dc38de542078
billy <billy@17dcompd006.ncl.ac.uk>
2023-06-22 18:31:58 +0100

    Forgot to commit updates to channel gatherer script.

--- channel_gatherer.py
+++ channel_gatherer.py
@@ -1,9 +1,9 @@
 import pyedflib
 import os
-
+import constants
 
 if __name__ == "__main__":
-	root = "../UCLH"
+	root = "/data/UCLH/icEEG"	
 
 	edf_files = []
 	unreadable_files = []
@@ -14,18 +14,43 @@ if __name__ == "__main__":
 			if file.split(".")[-1].lower() in ["edf", "edf+", "bdf", "bdf+"]:
 				edf_files.append(os.path.join(path, file))
 
+	"""
 	# read headers and store in dict of filepath -> header
-	channels = set()
+	ecg_channels = set()
 	i = 0
 	for file in edf_files:
 		print(f"{i}/{len(edf_files)}")
 		try:
 			header = pyedflib.highlevel.read_edf_header(file, read_annotations=False)
-			channels = channels.union(set(header["channels"]))
+			channels = header["channels"]
+			channels = [ch for ch in channels if ("ecg" in ch.lower()) or ("ekg" in ch.lower())]
+			ecg_channels = ecg_channels.union(set(channels))
+		except OSError as e:
+			print(f"Could not read {file} ({e}).")
+
+		i += 1
+	print(ecg_channels)
+	"""
+
+	channel_to_files = {}
+
+	i = 0
+	for file in edf_files:
+		print(f"{i}/{len(edf_files)}")
+		try:
+			header = pyedflib.highlevel.read_edf_header(file, read_annotations=False)
+			channels = header["channels"]
+			ecg_channels = [ch for ch in channels if ("ecg" in ch.lower()) or ("ekg" in ch.lower())]
+			for ch in ecg_channels:
+				if ch in channel_to_files.keys():
+					channel_to_files[ch] = channel_to_files[ch] + [file]
+				else:
+					channel_to_files[ch] = [file]
+
 		except OSError as e:
 			print(f"Could not read {file} ({e}).")
 
 		i += 1
 
-	print([ch for ch in channels if ("ecg" in ch.lower()) or ("ekg" in ch.lower())])
+	print(channel_to_files)
 
